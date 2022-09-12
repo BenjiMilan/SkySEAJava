965ae35303fd708da8890d4b30d077695ef9533f
Benji Milan <benjamin.milan@sky.uk>
2022-09-12 10:08:24 +0100

    Integer, character and boolean added

--- src/SayHeelloo.java
+++ src/SayHeelloo.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class SayHeelloo {
     public static void main(String[] args) {
+        int myInteger = 5;
+        char myCharacter = 'a';
+        boolean myBoolean = false;
         System.out.println("Hello");
+        System.out.println(myInteger);
+        System.out.println(myCharacter);
+        System.out.println(myBoolean);
     }
 }
