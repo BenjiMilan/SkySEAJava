0e971a581dbecde86fc4a301377fe2282db47d86
Benji Milan <benjamin.milan@sky.uk>
2022-09-12 12:21:54 +0100

    Added arithmetic demo

--- src/SayHeelloo.java
+++ src/SayHeelloo.java
@@ -9,5 +9,19 @@ public class SayHeelloo {
         System.out.println(myInteger);
         System.out.println(myCharacter);
         System.out.println(myBoolean);
+
+        int x = 100;
+        int y = 5;
+        int z = 30;
+        int result = x*y*z;
+        System.out.println(result);
+
+        String street = "1600 Pennsylvania Avenue NW";
+        String city = "Washington";
+        String state = "DC";
+        int zip = 20500;
+        String whiteHouseAddress = street + ", " + city + ", " + state + ", " + zip;
+        System.out.println(whiteHouseAddress);
+
     }
 }
